a86066d84199d104cfdf393ca5238131c1240512
Trần minh tuấn <tuan260387@gmail.com>
2018-04-18 23:48:18 +0700

    aaaa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
+
 const bodyParser  = require('body-parser');
 const morgan      = require('morgan');
 const mongoose    = require('mongoose');
@@ -14,6 +15,55 @@ const app = express();
 //ejs
 app.set('view engine','ejs');
 
+app.get('/',(rep,res)=>res.render('index'));
+
+//set storage engine
+const storage  = multer.diskStorage({
+    destination:'./public/uploadchat/',
+    filename:function (req, file, cb) {
+        cb(null,file.fieldname +'-'+Date.now()+
+            path.extname(file.originalname));
+    }
+});
+
+//Init Upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize:1240000},
+    fileFilter:function (file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+//check file type
+function checkFileType(file, cb){
+    //allowed extfileTypes
+    const filetypes = /jpeg|jpg|png|gif/;
+    //check ext
+    const extname = filetypes.test( path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        return cb(null, true);
+    }else{
+        cb('Error: Images Only');
+    }
+}
+
+//api upload
+app.post('/uploadchat',(req, res)=>{
+    upload(req, res, (err)=>{
+        if (err){
+            res.render('index',{
+                msg: err
+            });
+        }  else{
+            console.log(req.file);
+
+            res.send(req.file.path);
+        }
+    });
+});
 
 
 //public
@@ -30,32 +80,3 @@ app.use(morgan('dev'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 
-app.get('/',(rep,res)=>res.render('index'));
-
-//set storage engine
-const storage = multer.diskStorage({
-    description:'./public/uploadchat/',
-    fileName:function (req, file, cb) {
-        cb(null,file.fieldname+'-'+Date.now()+
-            path.extname(file.originalname));
-    }
-});
-
-//Init Upload
-const upload = multer({
-    storage:storage
-}).single('myImage');
-
-//api upload
-app.post('/uploadchat',(req, res)=>{
-    upload(req, res, (err)=>{
-       if (err){
-           res.render('index',{
-               msg: err
-           });
-       }  else{
-           console.log(req.file);
-           res.send('test');
-       }
-    });
-});
